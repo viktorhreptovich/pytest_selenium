2a3ff14a71f831c2add67454cc1767fef49474d0
Viktor Hreptovich <vhreptovich@gmail.com>
2021-04-13 20:20:38 +0300

    add locators

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -8,7 +8,6 @@ def test_guest_can_go_to_login_page(browser):
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = MainPage(browser, 'http://selenium1py.pythonanywhere.com/')
     page.open()
     page.should_be_login_link()
