15b1029c460de707e1487c9b5df28f380fd31290
Viktor Hreptovich <vhreptovich@gmail.com>
2021-04-13 20:51:25 +0300

    add login page

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,10 +1,12 @@
+from pages.login_page import LoginPage
 from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, 'http://selenium1py.pythonanywhere.com/')
-    page.open()
-    page.go_to_login_page()
+    main_page = MainPage(browser, 'http://selenium1py.pythonanywhere.com/')
+    main_page.open()
+    login_page = main_page.go_to_login_page()
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
