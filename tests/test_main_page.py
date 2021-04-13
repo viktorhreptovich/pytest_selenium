5bf086f891364ac98ce2c79747f9b6b1ae35c5ae
Viktor Hreptovich <vhreptovich@gmail.com>
2021-04-13 20:06:39 +0300

    test login link

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,6 +1,12 @@
 from pages.main_page import MainPage
 
 
+def test_guest_can_go_to_login_page(browser):
+    page = MainPage(browser, 'http://selenium1py.pythonanywhere.com/')
+    page.open()
+    page.go_to_login_page()
+
+
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
